0c231d635e7ad71609bb80abac5e0ade15ffb860
AlexsJones <alexsimonjones@gmail.com>
2023-03-27 20:49:40 +0100

    feat: version
    Signed-off-by: AlexsJones <alexsimonjones@gmail.com>

--- cmd/version.go
+++ cmd/version.go
@@ -16,14 +16,4 @@ var versionCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
