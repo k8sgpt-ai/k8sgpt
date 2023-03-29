85d0690d9097f40a4762bb0d32c57e08fac65678
AlexsJones <alexsimonjones@gmail.com>
2023-03-29 20:42:01 +0100

    wip UX improvements
    Signed-off-by: AlexsJones <alexsimonjones@gmail.com>

--- cmd/analyze/table.go
+++ cmd/analyze/table.go
@@ -5,12 +5,15 @@ import (
 	"os"
 
 	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 var (
-	WindowSize tea.WindowSizeMsg
+	WindowSize    tea.WindowSizeMsg
+	tableStyle    = lipgloss.NewStyle().Align(lipgloss.Top, lipgloss.Left).BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
+	viewportStyle = lipgloss.NewStyle().Align(lipgloss.Bottom, lipgloss.Left).BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
 )
 
 type MainModel struct {
@@ -18,6 +21,7 @@ type MainModel struct {
 
 type model struct {
 	table table.Model
+	view  viewport.Model
 	rows  []table.Row
 }
 
@@ -28,10 +32,15 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+
 	case tea.WindowSizeMsg:
 		WindowSize = msg
 		m.table.SetWidth(WindowSize.Width)
-		m.table.SetHeight(WindowSize.Height - (WindowSize.Height / 9))
+		m.table.SetHeight(WindowSize.Height / 2)
+		tableStyle = lipgloss.NewStyle().Width(WindowSize.Width).Height(WindowSize.Height/2).Align(lipgloss.Top, lipgloss.Left).BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
+		viewportStyle = lipgloss.NewStyle().Width(WindowSize.Width).Height(WindowSize.Height/3).Align(lipgloss.Bottom, lipgloss.Left).BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
+		m.view = viewport.New(WindowSize.Width, WindowSize.Height/3)
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
@@ -43,20 +52,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "enter":
-			entry := InitEntry(m.table.SelectedRow(), m.rows)
-			return entry.Update(WindowSize)
+			//m.view.SetContent(m.table.SelectedRow()[4])
+		default:
+			m.view.SetContent(m.table.SelectedRow()[4])
 		}
 	}
 	m.table, cmd = m.table.Update(msg)
+
 	return m, cmd
 }
 
-var baseStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.NormalBorder()).
-	BorderForeground(lipgloss.Color("240"))
-
 func (m model) View() string {
-	return baseStyle.Render(m.table.View()) + "\n"
+	var s string
+	//s += tableStyle.Render(m.table.View())
+	s += lipgloss.JoinVertical(lipgloss.Top, tableStyle.Render(m.table.View()), viewportStyle.Render(m.view.View()))
+
+	//	s += viewportStyle.Render(m.view.View())
+	return s
 }
 
 func InitTable(rows []table.Row) (tea.Model, tea.Cmd) {
@@ -67,25 +79,19 @@ func InitTable(rows []table.Row) (tea.Model, tea.Cmd) {
 		{Title: "Error", Width: 100},
 		{Title: "Details", Width: 0},
 	}), table.WithRows(rows), table.WithFocused(true),
-		table.WithHeight(WindowSize.Height-(WindowSize.Height/9)),
+		table.WithHeight(WindowSize.Height/2),
 		table.WithWidth(WindowSize.Width))
 
 	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
 
 	t.SetStyles(s)
 
+	v := viewport.New(WindowSize.Width, WindowSize.Height/3)
+	v.SetContent(t.SelectedRow()[4])
 	m := model{
 		table: t,
 		rows:  rows,
+		view:  v,
 	}
 
 	return m, func() tea.Msg { return nil }
