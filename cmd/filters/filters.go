55632156a51d1a259e675aa383708e11e614c3a2
chetan gupta <chetangupta123raj@gmail.com>
2023-04-02 20:21:53 +0530

    added aliases filter
    Signed-off-by: chetan gupta <chetangupta123raj@gmail.com>

--- cmd/filters/filters.go
+++ cmd/filters/filters.go
@@ -6,7 +6,7 @@ import (
 
 var FiltersCmd = &cobra.Command{
 	Use:     "filters",
-	Aliases: []string{"filters"},
+	Aliases: []string{"filters", "filter"},
 	Short:   "Manage filters for analyzing Kubernetes resources",
 	Long: `The filters command allows you to manage filters that are used to analyze Kubernetes resources.
 	You can list available filters to analyze resources.`,
