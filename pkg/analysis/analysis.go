k8sgpt-ai/k8sgpt

commit ab55f157ef026502d29eadf5ad83e917fe085a6c

fix: exit progressbar on error (#99)
Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

pkg/analysis/analysis.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/fatih/color"
@@ -143,13 +142,14 @@ func (a *Analysis) GetAIResults(output string) error {
 	for index, analysis := range a.Results {
 		parsedText, err := a.AIClient.Parse(a.Context, analysis.Error, a.NoCache)
 		if err != nil {
+			bar.Exit()
+
 			// Check for exhaustion
 			if strings.Contains(err.Error(), "status code: 429") {
-				color.Red("Exhausted API quota. Please try again later")
-				os.Exit(1)
+				return fmt.Errorf("exhausted API quota for AI provider %s: %v", a.AIClient.GetName(), err)
+			} else {
+				return fmt.Errorf("failed while calling AI provider %s: %v", a.AIClient.GetName(), err)
 			}
-			color.Red("Error: %v", err)
-			continue
 		}
 		analysis.Details = parsedText
 		if output != "json" {

pkg/ai/openai.go
@@ -77,7 +77,6 @@ func (a *OpenAIClient) Parse(ctx context.Context, prompt []string, nocache bool)
 
 	response, err := a.GetCompletion(ctx, inputKey)
 	if err != nil {
-		color.Red("error getting completion: %v", err)
 		return "", err
 	}
 

commit cfe20851114a8d69b997dfc7d53b044e0567eb17

chore: fix mistake introduced by ab55f157
Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

pkg/analysis/analysis.go
@@ -142,7 +142,11 @@ func (a *Analysis) GetAIResults(output string) error {
 	for index, analysis := range a.Results {
 		parsedText, err := a.AIClient.Parse(a.Context, analysis.Error, a.NoCache)
 		if err != nil {
-			bar.Exit()
+			// FIXME: can we avoid checking if output is json multiple times?
+			//   maybe implement the progress bar better?
+			if output != "json" {
+				bar.Exit()
+			}
 
 			// Check for exhaustion
 			if strings.Contains(err.Error(), "status code: 429") {