26c0cb2eed75695220007e6d6f7b492c2641a149
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2023-04-13 13:29:19 +0200

    feat: add simple health endpoint
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -1,11 +1,11 @@
 package server
 
 import (
+	json "encoding/json"
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/analysis"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -16,6 +16,18 @@ type Config struct {
 	Key     string
 	Token   string
 	Output  string
+}
+
+type Health struct {
+	Status  string `json:"status"`
+	Success int    `json:"success"`
+	Failure int    `json:"failure"`
+}
+
+var health = Health{
+	Status:  "ok",
+	Success: 0,
+	Failure: 0,
 }
 
 type Result struct {
@@ -31,41 +43,55 @@ func (s *Config) analyzeHandler(w http.ResponseWriter, r *http.Request) {
 
 	config, err := analysis.NewAnalysis(s.Backend, language, []string{}, namespace, nocache, explain)
 	if err != nil {
+		health.Failure++
 		fmt.Fprintf(w, err.Error())
 	}
 
 	err = config.RunAnalysis()
 	if err != nil {
 		color.Red("Error: %v", err)
-		os.Exit(1)
+		health.Failure++
+		fmt.Fprintf(w, err.Error())
 	}
 
 	if explain {
 		err := config.GetAIResults(s.Output, anonymize)
 		if err != nil {
 			color.Red("Error: %v", err)
-			os.Exit(1)
+			health.Failure++
+			fmt.Fprintf(w, err.Error())
 		}
 	}
 
 	output, err := config.JsonOutput()
 	if err != nil {
 		color.Red("Error: %v", err)
-		os.Exit(1)
+		health.Failure++
+		fmt.Fprintf(w, err.Error())
 	}
+	health.Success++
 	fmt.Fprintf(w, string(output))
-
 }
 
 func (s *Config) Serve() error {
 	http.HandleFunc("/analyze", s.analyzeHandler)
-	color.Green("Starting server on port %d", s.Port)
+	http.HandleFunc("/healthz", s.healthzHandler)
+	color.Green("Starting server on port %s", s.Port)
 	err := http.ListenAndServe(":"+s.Port, nil)
 	if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		return err
 	}
 	return nil
+}
+
+func (s *Config) healthzHandler(w http.ResponseWriter, r *http.Request) {
+	js, err := json.MarshalIndent(health, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, string(js))
 }
 
 func getBoolParam(param string) bool {
