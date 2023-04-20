84a3cc05fb6e21b732ef777351b42db8045e1093
Aris Boutselis <aris.boutselis@senseon.io>
2023-04-20 16:26:12 +0100

    feat: add configuration interface to support customer providers
    Signed-off-by: Aris Boutselis <aris.boutselis@senseon.io>

--- pkg/ai/openai.go
+++ pkg/ai/openai.go
@@ -15,27 +15,22 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
-const (
-	default_prompt = "Simplify the following Kubernetes error message and provide a solution in %s: %s"
-	prompt_a       = "Read the following input %s and provide possible scenarios for remediation in %s"
-	prompt_b       = "Considering the following input from the Kubernetes resource %s and the error message %s, provide possible scenarios for remediation in %s"
-	prompt_c       = "Reading the following %s error message and it's accompanying log message %s, how would you simplify this message?"
-)
-
 type OpenAIClient struct {
 	client   *openai.Client
 	language string
 	model    string
 }
 
-func (c *OpenAIClient) Configure(token string, model string, language string) error {
-	client := openai.NewClient(token)
+func (c *OpenAIClient) Configure(config IAIConfig, language string) error {
+	token := config.GetPassword()
+	defaultConfig := openai.DefaultConfig(token)
+	client := openai.NewClientWithConfig(defaultConfig)
 	if client == nil {
 		return errors.New("error creating OpenAI client")
 	}
 	c.language = language
 	c.client = client
-	c.model = model
+	c.model = config.GetModel()
 	return nil
 }
 
