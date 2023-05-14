c231f8333f514ce6a1b313aa87b9490fa5903def
Camilo Giraldo <camigira@gmail.com>
2023-05-14 18:50:33 +1000

    fix: specific instructions for prompt output
    Signed-off-by: Camilo Giraldo <camigira@gmail.com>

--- pkg/ai/prompts.go
+++ pkg/ai/prompts.go
@@ -3,7 +3,7 @@ package ai
 const (
 	default_prompt = `Simplify the following Kubernetes error message delimited by triple dashes written in --- %s --- language; --- %s ---.
 	Provide the most possible solution in a step by step style in no more than 280 characters. Write the output in the following format:
-	Error:
-	Solution:
+	Error: {Explain error here}
+	Solution: {Step by step solution here}
 	`
 )
