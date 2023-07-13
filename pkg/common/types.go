731e1520ec071894e739660cd6673e834fe9f8fc
Alex Jones <alexsimonjones@gmail.com>
2023-07-13 08:54:22 +0100

    Revert "feat: add Validating/Mutating webhook analyzer (#548)" (#551)
    This reverts commit 750a10d44c59bc90de5241d1128ee74fa38bf350.

--- pkg/common/types.go
+++ pkg/common/types.go
@@ -20,7 +20,6 @@ import (
 	openapi_v2 "github.com/google/gnostic/openapiv2"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/ai"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/kubernetes"
-	regv1 "k8s.io/api/admissionregistration/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	autov1 "k8s.io/api/autoscaling/v1"
 	v1 "k8s.io/api/core/v1"
@@ -55,8 +54,6 @@ type PreAnalysis struct {
 	StatefulSet              appsv1.StatefulSet
 	NetworkPolicy            networkv1.NetworkPolicy
 	Node                     v1.Node
-	ValidatingWebhook        regv1.ValidatingWebhookConfiguration
-	MutatingWebhook          regv1.MutatingWebhookConfiguration
 	// Integrations
 	TrivyVulnerabilityReport trivy.VulnerabilityReport
 }
