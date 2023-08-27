08c2c1ce2dc5ec76a589e62162a12a8beea1e6e2
Alex Jones <alex@alexs-mbp.home>
2023-08-27 19:26:21 +0100

    chore: added more logging
    Signed-off-by: Alex Jones <alex@alexs-mbp.home>

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -3,7 +3,7 @@ package server
 import (
 	"context"
 	"errors"
-
+	"fmt"
 	schemav1 "buf.build/gen/go/k8sgpt-ai/k8sgpt/protocolbuffers/go/schema/v1"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/cache"
 )
@@ -14,10 +14,13 @@ func (h *handler) AddConfig(ctx context.Context, i *schemav1.AddConfigRequest) (
 		return nil, errors.New("BucketName & Region are required")
 	}
 
+
 	err := cache.AddRemoteCache(i.Cache.BucketName, i.Cache.Region)
 	if err != nil {
 		return &schemav1.AddConfigResponse{}, err
 	}
+
+	fmt.Println(fmt.Sprintf("Adding remote cache with bucket name: %s and region: %s", i.Cache.BucketName, i.Cache.Region))
 
 	return &schemav1.AddConfigResponse{
 		Status: "Configuration updated.",
@@ -31,6 +34,7 @@ func (h *handler) RemoveConfig(ctx context.Context, i *schemav1.RemoveConfigRequ
 		return &schemav1.RemoveConfigResponse{}, err
 	}
 
+	fmt.Println(fmt.Sprintf("Removing remote cache with bucket name: %s", i.Cache.BucketName))
 	return &schemav1.RemoveConfigResponse{
 		Status: "Successfully removed the remote cache",
 	}, nil
