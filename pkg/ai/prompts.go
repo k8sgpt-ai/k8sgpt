89315111dde437a8cabf7d8fe8c2bf516e98e559
Johannes Kleinlercher <johannes@kleinlercher.at>
2023-09-12 16:55:00 +0200

    feat: show each ConfigAuditReport check
    Signed-off-by: Johannes Kleinlercher <johannes@kleinlercher.at>

--- pkg/ai/prompts.go
+++ pkg/ai/prompts.go
@@ -6,10 +6,12 @@ const (
 	Error: {Explain error here}
 	Solution: {Step by step solution here}
 	`
-	trivy_prompt = "Explain the following trivy scan result and the detail risk or root cause of the CVE ID, then provide a solution. Response in %s: %s"
+	trivy_vuln_prompt = "Explain the following trivy scan result and the detail risk or root cause of the CVE ID, then provide a solution. Response in %s: %s"
+	trivy_conf_prompt = "Explain the following trivy scan result and the detail risk or root cause of the security check, then provide a solution."
 )
 
 var PromptMap = map[string]string{
 	"default":             default_prompt,
-	"VulnerabilityReport": trivy_prompt, // for Trivy integration, the key should match `Result.Kind` in pkg/common/types.go
+	"VulnerabilityReport": trivy_vuln_prompt, // for Trivy integration, the key should match `Result.Kind` in pkg/common/types.go
+	"ConfigAuditReport":   trivy_conf_prompt,
 }
