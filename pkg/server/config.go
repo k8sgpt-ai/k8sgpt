69fe2db8acb795add27f04c1c8ee8d05819300ac
Alex Jones <alexsimonjones@gmail.com>
2023-09-28 07:43:05 +0100

    feat: integration refactor (#684)
    * feat: more significant refactor
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    * feat: more significant refactor
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    * feat: reworked the integration activate/deactivation
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    * chore: updated schema for list integrations
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    * fix: error with incorrect error being swallowed
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    * feat: added namespace check
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    * chore: fixed issue with namespace and skip install validation
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>
    
    ---------
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -1,63 +1,44 @@
 package server
 
 import (
-	"context"
-	"errors"
-
 	schemav1 "buf.build/gen/go/k8sgpt-ai/k8sgpt/protocolbuffers/go/schema/v1"
-	"github.com/k8sgpt-ai/k8sgpt/pkg/analyzer"
+	"context"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/cache"
-	"github.com/k8sgpt-ai/k8sgpt/pkg/integration"
-	"github.com/spf13/viper"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (h *handler) AddConfig(ctx context.Context, i *schemav1.AddConfigRequest) (*schemav1.AddConfigResponse, error,
 ) {
 
-	if i.Integrations != nil {
-		coreFilters, _, _ := analyzer.ListFilters()
-		// Update filters
-		activeFilters := viper.GetStringSlice("active_filters")
-		if len(activeFilters) == 0 {
-			activeFilters = coreFilters
-		}
-		integration := integration.NewIntegration()
-
-		if i.Integrations.Trivy != nil {
-			// Enable/Disable Trivy
-			var err = integration.Activate("trivy", i.Integrations.Trivy.Namespace,
-				activeFilters, i.Integrations.Trivy.SkipInstall)
-			return &schemav1.AddConfigResponse{
-				Status: "",
-			}, err
-		}
+	resp, err := h.syncIntegration(ctx, i)
+	if err != nil {
+		return resp, err
 	}
+
 	if i.Cache != nil {
 		// Remote cache
 		if i.Cache.BucketName == "" || i.Cache.Region == "" {
-			return &schemav1.AddConfigResponse{}, errors.New("BucketName & Region are required")
+			return resp, status.Error(codes.InvalidArgument, "cache arguments")
 		}
 
 		err := cache.AddRemoteCache(i.Cache.BucketName, i.Cache.Region)
 		if err != nil {
-			return &schemav1.AddConfigResponse{
-				Status: err.Error(),
-			}, err
+			return resp, err
 		}
 	}
-	return &schemav1.AddConfigResponse{
-		Status: "Configuration updated.",
-	}, nil
+	return resp, nil
 }
 
 func (h *handler) RemoveConfig(ctx context.Context, i *schemav1.RemoveConfigRequest) (*schemav1.RemoveConfigResponse, error,
 ) {
 	err := cache.RemoveRemoteCache(i.Cache.BucketName)
 	if err != nil {
-		return &schemav1.RemoveConfigResponse{
-			Status: err.Error(),
-		}, err
+		return &schemav1.RemoveConfigResponse{}, err
 	}
+
+	// Remove any integrations is a TBD as it would be nice to make this more granular
+	// Currently integrations can be removed in the AddConfig sync
 
 	return &schemav1.RemoveConfigResponse{
 		Status: "Successfully removed the remote cache",
--- pkg/server/integration.go
+++ pkg/server/integration.go
@@ -1,24 +1,144 @@
 package server
 
 import (
-	"context"
-
 	schemav1 "buf.build/gen/go/k8sgpt-ai/k8sgpt/protocolbuffers/go/schema/v1"
+	"context"
+	"fmt"
+	"github.com/k8sgpt-ai/k8sgpt/pkg/analyzer"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/integration"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const (
+	trivyName = "trivy"
+)
+
+// syncIntegration is aware of the following events
+// A new integration added
+// An integration removed from the Integration block
+func (h *handler) syncIntegration(ctx context.Context,
+	i *schemav1.AddConfigRequest) (*schemav1.AddConfigResponse, error,
+) {
+	response := &schemav1.AddConfigResponse{}
+	integrationProvider := integration.NewIntegration()
+	if i.Integrations == nil {
+		// If there are locally activate integrations, disable them
+		err := h.deactivateAllIntegrations(integrationProvider)
+		if err != nil {
+			return response, status.Error(codes.NotFound, "deactivation error")
+		}
+		return response, nil
+	}
+	coreFilters, _, _ := analyzer.ListFilters()
+	// Update filters
+	activeFilters := viper.GetStringSlice("active_filters")
+	if len(activeFilters) == 0 {
+		activeFilters = coreFilters
+	}
+	var err error = status.Error(codes.OK, "")
+	deactivateFunc := func(integrationRef integration.IIntegration) error {
+		namespace, err := integrationRef.GetNamespace()
+		if err != nil {
+			return err
+		}
+		err = integrationProvider.Deactivate(trivyName, namespace)
+		if err != nil {
+			return status.Error(codes.NotFound, "integration already deactivated")
+		}
+		return nil
+	}
+	integrationRef, err := integrationProvider.Get(trivyName)
+	if err != nil {
+		return response, status.Error(codes.NotFound, "provider get failure")
+	}
+	if i.Integrations.Trivy != nil {
+		switch i.Integrations.Trivy.Enabled {
+		case true:
+			if b, err := integrationProvider.IsActivate(trivyName); err != nil {
+				return response, status.Error(codes.Internal, "integration activation error")
+			} else {
+				if !b {
+					err := integrationProvider.Activate(trivyName, i.Integrations.Trivy.Namespace,
+						activeFilters, i.Integrations.Trivy.SkipInstall)
+					if err != nil {
+						return nil, err
+					}
+				} else {
+					return response, status.Error(codes.AlreadyExists, "integration already active")
+				}
+			}
+		case false:
+			err = deactivateFunc(integrationRef)
+			if err != nil {
+				return nil, err
+			}
+			// This break is included purely for static analysis to pass
+		}
+	} else {
+		// If Trivy has been removed, disable it
+		err = deactivateFunc(integrationRef)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return response, err
+}
 
 func (*handler) ListIntegrations(ctx context.Context, req *schemav1.ListIntegrationsRequest) (*schemav1.ListIntegrationsResponse, error) {
 
 	integrationProvider := integration.NewIntegration()
-	integrations := integrationProvider.List()
-	resp := &schemav1.ListIntegrationsResponse{
-		Integrations: make([]string, 0),
+	// Update the requester with the status of Trivy
+	trivy, err := integrationProvider.Get(trivyName)
+	active := trivy.IsActivate()
+	var skipInstall bool
+	var namespace string = ""
+	if active {
+		namespace, err = trivy.GetNamespace()
+		if err != nil {
+			return nil, status.Error(codes.NotFound, "namespace not found")
+		}
+		if namespace == "" {
+			skipInstall = true
+		}
 	}
+
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "trivy integration")
+	}
+	resp := &schemav1.ListIntegrationsResponse{
+		Trivy: &schemav1.Trivy{
+			Enabled:     active,
+			Namespace:   namespace,
+			SkipInstall: skipInstall,
+		},
+	}
+
+	return resp, nil
+}
+
+func (*handler) deactivateAllIntegrations(integrationProvider *integration.Integration) error {
+	integrations := integrationProvider.List()
 	for _, i := range integrations {
 		b, _ := integrationProvider.IsActivate(i)
 		if b {
-			resp.Integrations = append(resp.Integrations, i)
+			in, err := integrationProvider.Get(i)
+			namespace, err := in.GetNamespace()
+			if err != nil {
+				return err
+			}
+			if err == nil {
+				if namespace != "" {
+					integrationProvider.Deactivate(i, namespace)
+				} else {
+					fmt.Printf("Skipping deactivation of %s, not installed\n", i)
+				}
+			} else {
+				return err
+			}
 		}
 	}
-	return resp, nil
+	return nil
 }
