9879d5b34f3ea490df9bba3956a1ab41411f1988
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-02-23 10:25:35 +0530

    fix: multiple time add default backend error
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- cmd/auth/add.go
+++ cmd/auth/add.go
@@ -47,21 +47,11 @@ var addCmd = &cobra.Command{
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-
 		// get ai configuration
 		err := viper.UnmarshalKey("ai", &configAI)
 		if err != nil {
 			color.Red("Error: %v", err)
 			os.Exit(1)
-		}
-
-		// search for provider with same name
-		providerIndex := -1
-		for i, provider := range configAI.Providers {
-			if backend == provider.Name {
-				providerIndex = i
-				break
-			}
 		}
 
 		validBackend := func(validBackends []string, backend string) bool {
@@ -107,6 +97,15 @@ var addCmd = &cobra.Command{
 				os.Exit(1)
 			}
 			password = strings.TrimSpace(string(bytePassword))
+		}
+
+		// search for provider with same name
+		providerIndex := -1
+		for i, provider := range configAI.Providers {
+			if backend == provider.Name || defaultBackend == provider.Name {
+				providerIndex = i
+				break
+			}
 		}
 
 		// create new provider object
