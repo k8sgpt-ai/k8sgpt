8433281c4a5277191249854983cbb4afe392b9ee
VaibhavMalik4187 <vaibhavmalik2018@gmail.com>
2024-03-16 11:45:00 +0530

    test: added missing tests for the CronJob analyzer
    - Fixed a small bug where pre-analysis was incorrectly appended to the
      results every time at the end of the for loop. This caused the result
      for a single cronjob failure to be appended multiple times in the
      final results.
    
    - Added missing test cases to ensure proper testing of the CronJob
      analyzer. The addition of these missing test cases has increased the
      code coverage of this analyzer to over 96%.
    
    Partially Addresses: https://github.com/k8sgpt-ai/k8sgpt/issues/889
    
    Signed-off-by: VaibhavMalik4187 <vaibhavmalik2018@gmail.com>

--- pkg/analyzer/cronjob_test.go
+++ pkg/analyzer/cronjob_test.go
@@ -15,219 +15,165 @@ package analyzer
 
 import (
 	"context"
+	"sort"
 	"testing"
 
 	"github.com/k8sgpt-ai/k8sgpt/pkg/common"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/kubernetes"
-	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/require"
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestCronJobSuccess(t *testing.T) {
-	clientset := fake.NewSimpleClientset(&batchv1.CronJob{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "example-cronjob",
-			Namespace: "default",
-			Annotations: map[string]string{
-				"analysisDate": "2022-04-01",
-			},
-			Labels: map[string]string{
-				"app": "example-app",
-			},
-		},
-		Spec: batchv1.CronJobSpec{
-			Schedule:          "*/1 * * * *",
-			ConcurrencyPolicy: "Allow",
-			JobTemplate: batchv1.JobTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "example-app",
-					},
-				},
-				Spec: batchv1.JobSpec{
-					Template: v1.PodTemplateSpec{
-						Spec: v1.PodSpec{
-							Containers: []v1.Container{
-								{
-									Name:  "example-container",
-									Image: "nginx",
-								},
-							},
-							RestartPolicy: v1.RestartPolicyOnFailure,
-						},
-					},
-				},
-			},
-		},
-	})
+func TestCronJobAnalyzer(t *testing.T) {
+	suspend := new(bool)
+	*suspend = true
+
+	startingDeadline := new(int64)
+	*startingDeadline = -7
+
+	positiveStartingDeadline := new(int64)
+	*positiveStartingDeadline = 7
 
 	config := common.Analyzer{
 		Client: &kubernetes.Client{
-			Client: clientset,
-		},
-		Context:   context.Background(),
-		Namespace: "default",
-	}
-
-	analyzer := CronJobAnalyzer{}
-	analysisResults, err := analyzer.Analyze(config)
-	if err != nil {
-		t.Error(err)
-	}
-
-	assert.Equal(t, len(analysisResults), 0)
-}
-
-func TestCronJobBroken(t *testing.T) {
-	clientset := fake.NewSimpleClientset(&batchv1.CronJob{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "example-cronjob",
-			Namespace: "default",
-			Annotations: map[string]string{
-				"analysisDate": "2022-04-01",
-			},
-			Labels: map[string]string{
-				"app": "example-app",
-			},
-		},
-		Spec: batchv1.CronJobSpec{
-			Schedule:          "*** * * * *",
-			ConcurrencyPolicy: "Allow",
-			JobTemplate: batchv1.JobTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "example-app",
-					},
-				},
-				Spec: batchv1.JobSpec{
-					Template: v1.PodTemplateSpec{
-						Spec: v1.PodSpec{
-							Containers: []v1.Container{
-								{
-									Name:  "example-container",
-									Image: "nginx",
-								},
-							},
-							RestartPolicy: v1.RestartPolicyOnFailure,
-						},
-					},
-				},
-			},
-		},
-	})
-
-	config := common.Analyzer{
-		Client: &kubernetes.Client{
-			Client: clientset,
-		},
-		Context:   context.Background(),
-		Namespace: "default",
-	}
-
-	analyzer := CronJobAnalyzer{}
-	analysisResults, err := analyzer.Analyze(config)
-	if err != nil {
-		t.Error(err)
-	}
-
-	assert.Equal(t, len(analysisResults), 1)
-	assert.Equal(t, analysisResults[0].Name, "default/example-cronjob")
-	assert.Equal(t, analysisResults[0].Kind, "CronJob")
-}
-
-func TestCronJobBrokenMultipleNamespaceFiltering(t *testing.T) {
-	clientset := fake.NewSimpleClientset(
-		&batchv1.CronJob{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "example-cronjob",
-				Namespace: "default",
-				Annotations: map[string]string{
-					"analysisDate": "2022-04-01",
-				},
-				Labels: map[string]string{
-					"app": "example-app",
-				},
-			},
-			Spec: batchv1.CronJobSpec{
-				Schedule:          "*** * * * *",
-				ConcurrencyPolicy: "Allow",
-				JobTemplate: batchv1.JobTemplateSpec{
+			Client: fake.NewSimpleClientset(
+				&batchv1.CronJob{
 					ObjectMeta: metav1.ObjectMeta{
+						Name: "CJ1",
+						// This CronJob won't be list because of namespace filtering.
+						Namespace: "test",
+					},
+				},
+				&batchv1.CronJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "CJ2",
+						Namespace: "default",
+					},
+					// A suspended CronJob will contribute to failures.
+					Spec: batchv1.CronJobSpec{
+						Suspend: suspend,
+					},
+				},
+				&batchv1.CronJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "CJ3",
+						Namespace: "default",
+					},
+					Spec: batchv1.CronJobSpec{
+						// Valid schedule
+						Schedule: "*/1 * * * *",
+
+						// Negative starting deadline
+						StartingDeadlineSeconds: startingDeadline,
+					},
+				},
+				&batchv1.CronJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "CJ4",
+						Namespace: "default",
+					},
+					Spec: batchv1.CronJobSpec{
+						// Invalid schedule
+						Schedule: "*** * * * *",
+					},
+				},
+				&batchv1.CronJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "CJ5",
+						Namespace: "default",
+					},
+					Spec: batchv1.CronJobSpec{
+						// Valid schedule
+						Schedule: "*/1 * * * *",
+
+						// Positive starting deadline shouldn't be any problem.
+						StartingDeadlineSeconds: positiveStartingDeadline,
+					},
+				},
+				&batchv1.CronJob{
+					// This cronjob shouldn't contribute to any failures.
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "successful-cronjob",
+						Namespace: "default",
+						Annotations: map[string]string{
+							"analysisDate": "2022-04-01",
+						},
 						Labels: map[string]string{
 							"app": "example-app",
 						},
 					},
-					Spec: batchv1.JobSpec{
-						Template: v1.PodTemplateSpec{
-							Spec: v1.PodSpec{
-								Containers: []v1.Container{
-									{
-										Name:  "example-container",
-										Image: "nginx",
+					Spec: batchv1.CronJobSpec{
+						Schedule:          "*/1 * * * *",
+						ConcurrencyPolicy: "Allow",
+						JobTemplate: batchv1.JobTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Labels: map[string]string{
+									"app": "example-app",
+								},
+							},
+							Spec: batchv1.JobSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											{
+												Name:  "example-container",
+												Image: "nginx",
+											},
+										},
+										RestartPolicy: v1.RestartPolicyOnFailure,
 									},
 								},
-								RestartPolicy: v1.RestartPolicyOnFailure,
 							},
 						},
 					},
 				},
-			},
-		},
-		&batchv1.CronJob{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "example-cronjob",
-				Namespace: "other-namespace",
-				Annotations: map[string]string{
-					"analysisDate": "2022-04-01",
-				},
-				Labels: map[string]string{
-					"app": "example-app",
-				},
-			},
-			Spec: batchv1.CronJobSpec{
-				Schedule:          "*** * * * *",
-				ConcurrencyPolicy: "Allow",
-				JobTemplate: batchv1.JobTemplateSpec{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: map[string]string{
-							"app": "example-app",
-						},
-					},
-					Spec: batchv1.JobSpec{
-						Template: v1.PodTemplateSpec{
-							Spec: v1.PodSpec{
-								Containers: []v1.Container{
-									{
-										Name:  "example-container",
-										Image: "nginx",
-									},
-								},
-								RestartPolicy: v1.RestartPolicyOnFailure,
-							},
-						},
-					},
-				},
-			},
-		})
-
-	config := common.Analyzer{
-		Client: &kubernetes.Client{
-			Client: clientset,
+			),
 		},
 		Context:   context.Background(),
 		Namespace: "default",
 	}
 
-	analyzer := CronJobAnalyzer{}
-	analysisResults, err := analyzer.Analyze(config)
-	if err != nil {
-		t.Error(err)
+	cjAnalyzer := CronJobAnalyzer{}
+	results, err := cjAnalyzer.Analyze(config)
+	require.NoError(t, err)
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
+	})
+
+	expectations := []struct {
+		name         string
+		failuresText []string
+	}{
+		{
+			name: "default/CJ2",
+			failuresText: []string{
+				"CronJob CJ2 is suspended",
+			},
+		},
+		{
+			name: "default/CJ3",
+			failuresText: []string{
+				"CronJob CJ3 has a negative starting deadline",
+			},
+		},
+		{
+			name: "default/CJ4",
+			failuresText: []string{
+				"CronJob CJ4 has an invalid schedule",
+			},
+		},
 	}
 
-	assert.Equal(t, len(analysisResults), 1)
-	assert.Equal(t, analysisResults[0].Name, "default/example-cronjob")
-	assert.Equal(t, analysisResults[0].Kind, "CronJob")
+	require.Equal(t, len(expectations), len(results))
+
+	for i, result := range results {
+		require.Equal(t, expectations[i].name, results[i].Name)
+		for j, failure := range result.Error {
+			require.Contains(t, failure.Text, expectations[i].failuresText[j])
+		}
+	}
 }
