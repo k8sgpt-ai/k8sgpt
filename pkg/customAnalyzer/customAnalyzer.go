108424bc1ff733e94118d1dcc791182f8c1773f1
Matthis Holleville <matthish29@gmail.com>
2024-08-05 19:36:42 +0200

    feat: Add name validation for custom analyzer creation
    To ensure the integrity and consistency of analyzer names, we introduced a validation step that checks the format of the name against a predefined regex pattern. This change aims to prevent the creation of analyzers with invalid names, enhancing the system's reliability and usability.
    
    Signed-off-by: Matthis Holleville <matthish29@gmail.com>

--- pkg/customAnalyzer/customAnalyzer.go
+++ pkg/customAnalyzer/customAnalyzer.go
@@ -3,6 +3,7 @@ package customanalyzer
 import (
 	"fmt"
 	"reflect"
+	"regexp"
 )
 
 type CustomAnalyzerConfiguration struct {
@@ -22,6 +23,12 @@ func NewCustomAnalyzer() *CustomAnalyzer {
 }
 
 func (*CustomAnalyzer) Check(actualConfig []CustomAnalyzerConfiguration, name, url string, port int) error {
+	validNameRegex := `^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$`
+	validName := regexp.MustCompile(validNameRegex)
+	if !validName.MatchString(name) {
+		return fmt.Errorf("invalid name format. Must match %s", validNameRegex)
+	}
+	
 	for _, analyzer := range actualConfig {
 		if analyzer.Name == name {
 			return fmt.Errorf("custom analyzer with the name '%s' already exists. Please use a different name", name)
