0006226c4767dfb55c727c2a1656cac53d1f54e0
naveenthangaraj03 <tnaveen3402@gmail.com>
2024-09-20 13:49:27 +0530

    getting the error from the event for sts

--- pkg/analyzer/statefulset.go
+++ pkg/analyzer/statefulset.go
@@ -21,6 +21,7 @@ import (
 	"github.com/k8sgpt-ai/k8sgpt/pkg/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 type StatefulSetAnalyzer struct{}
@@ -93,6 +94,41 @@ func (StatefulSetAnalyzer) Analyze(a common.Analyzer) ([]common.Result, error) {
 				}
 			}
 		}
+		if  sts.Spec.Replicas != nil && *(sts.Spec.Replicas) != sts.Status.AvailableReplicas {
+			for i := int32(0) ; i < *(sts.Spec.Replicas) ; i++ {
+				podName := sts.Name +"-"+ fmt.Sprint(i);
+				pod, err := a.Client.GetClient().CoreV1().Pods(sts.Namespace).Get(a.Context, podName , metav1.GetOptions{})
+				if err != nil {
+					if errors.IsNotFound(err) && i == 0{
+						evt, err := util.FetchLatestEvent(a.Context, a.Client, sts.Namespace, sts.Name)
+						if err != nil || evt == nil || evt.Type == "Normal" {
+							break
+						}
+						failures = append(failures, common.Failure{
+							Text: evt.Message,
+							Sensitive: []common.Sensitive{},
+						})
+					}
+					break
+				}
+				if pod.Status.Phase != "Running" {
+					failures = append(failures, common.Failure{
+						Text: fmt.Sprintf("Statefulset pod %s in the namespace %s is not in running state.", pod.Name, pod.Namespace),
+						Sensitive: []common.Sensitive{
+							{
+								Unmasked: sts.Namespace,
+								Masked:   util.MaskString(pod.Name),
+							},
+							{
+								Unmasked: serviceName,
+								Masked:   util.MaskString(pod.Namespace),
+							},
+						},
+					})
+					break
+				}
+			}
+	    }
 		if len(failures) > 0 {
 			preAnalysis[fmt.Sprintf("%s/%s", sts.Namespace, sts.Name)] = common.PreAnalysis{
 				StatefulSet:    sts,
