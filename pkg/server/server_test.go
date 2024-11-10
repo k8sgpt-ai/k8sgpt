0269458ba92f459ac84893a6f8a16d148864f2e4
AlexsJones <alexsimonjones@gmail.com>
2024-11-10 15:04:48 +0000

    feat: updated server test function
    Signed-off-by: AlexsJones <alexsimonjones@gmail.com>

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,38 +1,64 @@
 package server
 
 import (
-	"os"
+	"context"
+	"net"
 	"testing"
+	"time"
 
-	"github.com/fatih/color"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
 )
 
-func TestServerInit(t *testing.T) {
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		color.Red("failed to create logger: %v", err)
-		os.Exit(1)
-	}
-	//nolint:all
+func TestServe(t *testing.T) {
+	logger, _ := zap.NewDevelopment()
 	defer logger.Sync()
-	server_config := Config{
-		Backend:     "openai",
-		Port:        "0",
-		MetricsPort: "0",
-		Token:       "none",
-		Logger:      logger,
+
+	s := &Config{
+		Port:       "50059",
+		Logger:     logger,
+		EnableHttp: false,
 	}
 
 	go func() {
-		err := server_config.Serve()
-		if err != nil {
-			assert.Fail(t, "serve: %s", err.Error())
-		}
-		err = server_config.Shutdown()
-		if err != nil {
-			assert.Fail(t, "shutdown: %s", err.Error())
-		}
+		err := s.Serve()
+		assert.NoError(t, err, "Serve should not return an error")
 	}()
+
+	// Wait until the server is ready to accept connections
+	err := waitForPort("localhost:50059", 10*time.Second)
+	assert.NoError(t, err, "Server should start without error")
+
+	conn, err := grpc.Dial("localhost:50059", grpc.WithInsecure())
+	assert.NoError(t, err, "Should be able to dial the server")
+	defer conn.Close()
+
+	// Test a simple gRPC reflection request
+	cli := grpc_reflection_v1alpha.NewServerReflectionClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	resp, err := cli.ServerReflectionInfo(ctx)
+	assert.NoError(t, err, "Should be able to get server reflection info")
+	assert.NotNil(t, resp, "Response should not be nil")
+
+	// Cleanup
+	err = s.Shutdown()
+	assert.NoError(t, err, "Shutdown should not return an error")
+}
+
+func waitForPort(address string, timeout time.Duration) error {
+	start := time.Now()
+	for {
+		conn, err := net.Dial("tcp", address)
+		if err == nil {
+			conn.Close()
+			return nil
+		}
+		if time.Since(start) > timeout {
+			return err
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
